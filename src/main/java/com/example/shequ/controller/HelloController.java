e6d136ccfafe692eb640b6808280bee87fb99e0e
闹气 <1345546640@qq.com>
2020-01-17 15:58:49 +0800

    1.0

--- src/main/java/com/example/shequ/controller/HelloController.java
+++ src/main/java/com/example/shequ/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public String index(){
         return "index";
     }

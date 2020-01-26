daa80e8974f68032e368f9128f749d2f78c4b063
闹气 <1345546640@qq.com>
2020-01-26 17:46:39 +0800

    2.0

--- src/main/java/com/example/shequ/controller/AuthorizeController.java
+++ src/main/java/com/example/shequ/controller/AuthorizeController.java
@@ -1,4 +1,33 @@
 package com.example.shequ.controller;
 
+
+import com.example.shequ.dto.AccessTokenDTO;
+import com.example.shequ.dto.GithubUser;
+import com.example.shequ.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("273def90054da3e78121");
+        accessTokenDTO.setClient_secret("f561d8db05c47415d65e03cbe03f7ffb624b65d5");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
 }

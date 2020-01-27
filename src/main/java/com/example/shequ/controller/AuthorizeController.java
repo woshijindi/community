01eeedb2cd4595172f4971ed8a4aff03864fdfbf
闹气 <1345546640@qq.com>
2020-01-27 11:32:07 +0800

    3.0

--- src/main/java/com/example/shequ/controller/AuthorizeController.java
+++ src/main/java/com/example/shequ/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.example.shequ.dto.AccessTokenDTO;
 import com.example.shequ.dto.GithubUser;
 import com.example.shequ.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,16 +15,21 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
-
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("273def90054da3e78121");
-        accessTokenDTO.setClient_secret("f561d8db05c47415d65e03cbe03f7ffb624b65d5");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
